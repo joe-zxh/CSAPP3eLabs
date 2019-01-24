d213f3377361a76cb12fadf640013a441aeae6c7
Unknown <178559145@qq.com>
2019-01-24 13:59:15 +0800

    重新整理了命名方式

--- lab3_target1/cl2Binary.s
+++ lab3_target1/cl2Binary.s
@@ -1,10 +1,10 @@
 
-level2.o：     文件格式 elf64-x86-64
+cl2Draft.o：     文件格式 elf64-x86-64
 
 
 Disassembly of section .text:
 
 0000000000000000 <.text>:
-   0:	48 83 ec 24          	sub    $0x24(36),%rsp
+   0:	48 83 ec 24          	sub    $0x24,%rsp
    4:	48 c7 c7 fa 97 b9 59 	mov    $0x59b997fa,%rdi
    b:	c3                   	retq   
--- lab3_target1/level2_2.s
+++
@@ -1,3 +0,0 @@
-add dif,%rsp
-mov 0x59b997fa, %rdi
-retq
